55ad2c36d5cc7b394dbd58085b0093d68db52352
Mark <marktempini@googlemail.com>
2019-07-06 18:17:12 +0100

    fog of war implemented

---
+++ Project_Alpha/Assets/Scripts/RenderEnemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderEnemy : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+
+            other.GetComponent<MeshRenderer>().enabled = true;
+
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+
+            other.GetComponent<MeshRenderer>().enabled = false;
+
+        }
+    } 
+}
