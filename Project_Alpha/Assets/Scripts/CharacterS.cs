2b4126e3d667adb486b8fb021068955c3a84727e
Mark <marktempini@googlemail.com>
2019-07-14 12:00:29 +0100

    Revert "Revert "Broke everything. Character select attempt""
    This reverts commit 17afce37d9d0bc05563a7f4ca9a1341e48fa3860.

---
+++ Project_Alpha/Assets/Scripts/CharacterS.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Character")]
+public class Character : ScriptableObject
+{
+
+    public string characterName = "Default";
+    public int startingHp = 100;
+
+    public Ability[] characterAbilities;
+}
